10e8af68fcf1a83f7ae380a4c82cf6323a02f498
Luan <luandsvt@gmail.com>
2024-10-05 17:24:49 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,23 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
 		// TODO Auto-generated method stub
 		
 		Department obj = new Department(1, "books");
+		Seller seller = new Seller(1, "Maria", "maria@gmail.com", new Date(), 3.000, obj);
 		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
